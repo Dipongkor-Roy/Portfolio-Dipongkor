ebb0e3a0419f6290db9fa4c67eee5c59ac36a9cb
Dipongkor-Roy <juniordipu@gmail.com>
2023-07-15 17:22:38 +0600

    avatar and resume added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 
 function App() {
-  const [darkMode,setDarkMode]=useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <div className={darkMode && "dark"}> 
+    <div className={darkMode && "dark"}>
       <Header darkMode={darkMode} setDarkMode={setDarkMode}></Header>
 
       <main className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-         <Hero/>
+        <Hero />
       </main>
     </div>
-    
   );
 }
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaFacebook, FaTwitter, FaLinkedin, FaArrowDown } from "react-icons/fa";
-import avatar from '../../assetsall/avatar.png'
+import avatar from "../../assetsall/avatar.png";
 const Hero = () => {
   const social = [
     {
@@ -21,27 +21,46 @@ const Hero = () => {
   ];
   return (
     <section className="min-h-screen flex flex-col justify-start items-cneter p-5 text-center">
-      <h2 className="text-5xl text-rose-600 uppercase font-bold  ">Dipongkor Roy</h2>
+      <h2 className="text-5xl text-rose-600 uppercase font-bold  ">
+        Dipongkor Roy
+      </h2>
       <h3 className="py-3 text-2xl">Full Stack Developer</h3>
       <p className="max-w-xl font-light text-gray-500">
         Hello <span className="animate-pulse text-4xl">👋</span>
-        Welcome to my site. I am a freelance full stack developer. I love to work
-        on Web app, Frontend and Backend technology
+        Welcome to my site. I am a freelance full stack developer. I love to
+        work on Web app, Frontend and Backend technology
       </p>
 
       {/* social icons  */}
       <div className="flex justify-evenly py-8 lg:py-16 text-3xl w-full md:w-1/3">
-        {
-            social.map(({id,link,icon})=>(
-                <a href={link} key={id}
-                target="_blank" rel="noopener noreferrer"
-                className="cursor-pointer duration-300 hover:text-rose-600">{icon}</a>
-            ))
-        }
+        {social.map(({ id, link, icon }) => (
+          <a
+            href={link}
+            key={id}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cursor-pointer duration-300 hover:text-rose-600"
+          >
+            {icon}
+          </a>
+        ))}
       </div>
       {/* avatar and resume */}
       <div>
-        <img src={avatar} alt="avatar" className="w-60 h-60 md:w-72 md:h-72 object-cover object-top" />
+        <div className="flex justify-center items-center ">
+          <img
+            src={avatar}
+            alt="avatar"
+            className="w-60 h-60 md:w-72 md:h-72 object-cover object-top bg-gradient-to-b from-rose-600 rounded-xl pt-5"
+          />
+        </div>
+        <a
+          href="/dipongkor-resume.pdf"
+          download={true}
+          className="flex justify-center items-center mt-10 py-2 bg-gradient-to-r from-rose-600 to-teal-500 text-white text-2xl rounded-lg"
+        >
+          Resume
+        </a>
       </div>
     </section>
   );
